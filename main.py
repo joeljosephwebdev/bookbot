f1a0a02deb117a08e3b73e06e416c28a20429bf0
shabushabu <joeljoseph7825@gmail.com>
2025-05-14 13:12:21 -0700

    refactor: moved stat functions to their own file and added args to accept book path

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
-import string
-
-def get_book_text(path: str) -> str:
-    with open(path) as f:
-        return f.read()
-
-def count_words(text: str) -> int:
-    words = text.split()
-    return len(words)
-
-def char_count(text: str) -> dict:
-    character_count = {char: 0 for char in string.ascii_lowercase}
-    
-    for char in text:
-        char_lower = char.lower()
-        if char_lower in character_count:
-            character_count[char_lower] += 1
-    
-    return sort_dict(character_count)
-
-def sort_dict(dictionary: dict) -> dict:
-    # Sort the dictionary by values in descending order
-    sorted_dict = dict(sorted(dictionary.items(), key=lambda item: item[1], reverse=True))
-    return sorted_dict
+import sys
+from stats import get_book_text, count_words, char_count
 
 
 if __name__ == '__main__':
-    book_path = 'books/frankenstein.txt'
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1) 
+    
+    book_path = sys.argv[1]
+    if not book_path.endswith('.txt'):
+        print("Please provide a valid .txt file.")
+        sys.exit(1)
+ 
     book = get_book_text(book_path)
     print(f"""
 --- The book we are analyzing is {book_path} ---
-It has {count_words(book)} words.
+{count_words(book)} words found in the document.
 
 Here is a character frequency breakdown:
 """)
